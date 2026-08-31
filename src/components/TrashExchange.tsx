f465c33f1e79b924f1d50f17604259cbd266a979
Nikkilite <Niko6697n@gmail.com>
2026-08-31 12:53:42 +0200

    Use placeholder data for trash exchange

--- src/components/TrashExchange.tsx
+++ src/components/TrashExchange.tsx
@@ -1,11 +1,14 @@
 import { Title } from '../components/Title'
 import { Button } from '../components/Buttons'
+import { useSession } from '../context/SessionContext'
 
 export function TrashExchange() {
+  const { trashAquired, playerOptions } = useSession()
+
   return ( 
     <div className="flex flex-col gap-4">
         <Title>Trash Exchange</Title>
-        <h5>Trash required: @CurrentSession.ItemHandler.TrashAvailable/@CurrentSession.HintHandler.GetHintCost()</h5>
+        <h5>Trash required: {trashAquired} all trash/{playerOptions.HintShopCost} without needed calc</h5>
         <Button className='self-start'>Purchase Hint</Button>
     </div>
   )
