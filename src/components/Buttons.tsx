be851d9722b296126bfd487fb29872d9a26db055
Nikkilite <Niko6697n@gmail.com>
2026-09-02 19:33:49 +0200

    Make disabled buttons look nicer

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -8,7 +8,7 @@ type ButtonProps = {
 export function Button({ variant = "small", className, ...props }: ButtonProps) {
   return (
     <button {...props} className={twMerge(
-      "disabled:cursor-not-allowed hover:cursor-pointer rounded-lg disabled:bg-zinc-300 bg-legacy-blue-300 border-legacy-blue-400 border hover:bg-legacy-blue-250 hover:border-legacy-blue-300 viking:disabled:bg-viking-beige-300 viking:disabled:border-viking-beige-400 viking:font-bold viking:text-viking-beige-200 viking:bg-viking-red-300 viking:border-viking-red-400 viking:hover:bg-viking-red-200 viking:hover:border-viking-red-300",
+      "disabled:cursor-not-allowed hover:cursor-pointer rounded-lg disabled:bg-zinc-300 disabled:border-zinc-400 disabled:opacity-70 bg-legacy-blue-300 border-legacy-blue-400 border hover:bg-legacy-blue-250 hover:border-legacy-blue-300 viking:disabled:bg-viking-beige-300 viking:disabled:border-viking-beige-400 viking:font-bold viking:text-viking-beige-200 viking:bg-viking-red-300 viking:border-viking-red-400 viking:hover:bg-viking-red-200 viking:hover:border-viking-red-300",
       getVariantStyles(variant),
       className 
     )}>{props.children}</button>
