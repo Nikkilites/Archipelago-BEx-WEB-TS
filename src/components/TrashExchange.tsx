7351f8aa4bc120f9442c8fc2ed6d4c97dc00f2fa
Nikkilite <Niko6697n@gmail.com>
2026-09-05 19:54:36 +0200

    Disable Trash Exchange

--- src/components/TrashExchange.tsx
+++ src/components/TrashExchange.tsx
@@ -1,15 +1,17 @@
 import { Title } from '../components/Title'
 import { Button } from '../components/Buttons'
-import { useSession } from '../context/SessionContext'
+//import { useSession } from '../context/SessionContext'
 
 export function TrashExchange() {
-  const { trashAquired, playerOptions } = useSession()
+//  const { trashAquired, playerOptions } = useSession()
+
+//  <h5>Trash required: {trashAquired} all trash/{playerOptions.HintShopCost} without needed calc</h5>
 
   return ( 
     <div className="flex flex-col gap-4">
         <Title>Trash Exchange</Title>
-        <h5>Trash required: {trashAquired} all trash/{playerOptions.HintShopCost} without needed calc</h5>
-        <Button className='self-start'>Purchase Hint</Button>
+        <h5>This page is currently not functional</h5>
+        <Button className='self-start' disabled={true}>Purchase Hint</Button>
     </div>
   )
 }
